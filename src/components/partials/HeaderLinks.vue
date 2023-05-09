18e1f64433f07ecc1758b2c23283e3824d988104
GiacomoArena <giacomoarena28@gmail.com>
2023-05-09 16:12:36 +0200

    make Header

--- src/components/partials/HeaderLinks.vue
+++ src/components/partials/HeaderLinks.vue
@@ -1,10 +1,22 @@
 
 <script>
+  import {store} from '../../data/store'
+  import {games,shop,pages,blog} from '../../data/db'
+
 export default {
   name:'HeaderLinks',
   props:{
     link:String,
   },
+  data(){
+    return{
+      store,
+      games,
+      shop,
+      pages,
+      blog,
+    }
+  }
 }
 </script>
 
@@ -14,7 +26,38 @@ export default {
     <li>
       <a v-if="link === 'home'" href="#">{{link}} </a>
       <a v-else-if="link === 'contact'" href="#">{{link}} </a>
-      <a v-else href="#">{{link}} <img src="../../assets/svg/c-down-arrow.svg" ></a>
+      <a v-else href="#">{{link}} <img src="../../assets/svg/c-down-arrow.svg" >
+
+      <span class="hover-container" v-if="link === 'games'" >
+        <ul v-for="(text, i) in games" :key="i">
+          <li>
+            <a href="#">{{text.text}}</a>
+          </li>
+        </ul>
+      </span>
+      <span class="hover-container" v-if="link === 'shop'" >
+        <ul v-for="(text, i) in shop" :key="i">
+          <li>
+            <a href="#">{{text.text}}</a>
+          </li>
+        </ul>
+      </span>
+      <span class="hover-container" v-if="link === 'pages'" >
+        <ul v-for="(text, i) in pages" :key="i">
+          <li>
+            <a href="#">{{text.text}}</a>
+          </li>
+        </ul>
+      </span>
+      <span class="hover-container" v-if="link === 'blog'" >
+        <ul v-for="(text, i) in blog" :key="i">
+          <li>
+            <a href="#">{{text.text}}</a>
+          </li>
+        </ul>
+      </span>
+    
+    </a>
     </li>
   </ul>
 
@@ -25,9 +68,34 @@ export default {
 ul{
   margin: 0 10px;
   list-style: none;
+  
   a{
+    position: relative;
     text-decoration: none;
     color: white;
+    padding: 15px 0;
+    &:hover{
+      color: $my-lightgreen;
+    }
+    &:hover .hover-container{
+      opacity: 1;
+      display: flex;
+    }
+    
+    
+    .hover-container{
+      padding: 30px 0;
+      width: 200px;
+      display: none;
+      flex-direction: column;
+      position: absolute;
+      background-color: $my-blue;
+      top: 40px;
+      right: -60px;
+      opacity: 0;
+      
+    }
+
   }
 }
 
---
+++ src/data/db.js
@@ -0,0 +1,82 @@
+
+const games=[
+  {
+    text:'games'
+  },
+  {
+    text:'games details'
+  },
+  {
+    text:'match details'
+  },
+]
+
+
+
+const shop=[
+  {
+    text:'shop'
+  },
+  {
+    text:'shop single '
+  },
+  {
+    text:'shop single 2'
+  },
+  {
+    text:'shop single 3'
+  },
+  {
+    text:'shop single 4'
+  },
+]
+
+
+const pages=[
+  {
+    text:'about us'
+  },
+  {
+    text:'team'
+  },
+  {
+    text:'player'
+  },
+  {
+    text:'upcoming matches'
+  },
+  {
+    text:'FROs'
+  },
+  {
+    text:'privacy policy'
+  },
+  {
+    text:'terms'
+  },
+  {
+    text:'privacy policy'
+  },
+  {
+    text:'terms'
+  },
+  {
+    text:'coming soon'
+  },
+  {
+    text:'error'
+  },
+]
+
+const blog=[
+  {
+    text:'blog'
+  },
+  {
+    text:'blog single'
+  },
+  
+]
+
+
+export {games,shop,pages,blog};
