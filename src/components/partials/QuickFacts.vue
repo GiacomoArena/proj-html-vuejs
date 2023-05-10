de55fb0ed434ac5bd5c2c588f8cd1551d1b572ee
GiacomoArena <giacomoarena28@gmail.com>
2023-05-10 11:05:15 +0200

    make numbers animations

--- src/components/partials/QuickFacts.vue
+++ src/components/partials/QuickFacts.vue
@@ -5,7 +5,21 @@
     name:'QuickFacts',
     data(){
       return{
-        
+        startNumber:0,
+        targets:100
+      }
+    },
+    methods:{
+      increaseNumber(){
+        const timer = setInterval(() => {
+          
+          if (this.startNumber != this.targets) {
+            this.startNumber ++
+          }
+          else{
+            clearInterval(timer)
+          }
+        }, 50);
       }
     }
   }
@@ -14,21 +28,25 @@
 
 <template>
 
-<section>
+<section @mouseover="increaseNumber()">
   <h1>Futio Quick Facts</h1>
   <div class="containeraa">
     <div class="row justify-content-center">
       <div class="col-2 me-3 card">
-        0
+        {{startNumber}}
+        <h5>Twich Stream</h5>
       </div>
       <div class="col-2 me-3 card">
-        0
+        {{startNumber}}
+        <h5>Total Games</h5>
       </div>
       <div class="col-2 me-3 card">
-        0
+        {{startNumber}}
+        <h5>Youtube Streams</h5>
       </div>
       <div class="col-2 me-3 card">
-        0
+        {{startNumber}}
+        <h5>Twich Stream</h5>
       </div>
     </div>
   </div>
@@ -45,11 +63,25 @@
     background-color: black;
     padding: 100px 0px;
     color: white;
+    h1{
+      padding-bottom: 40px;
+      font-size: 3rem;
+      font-weight: 600;
+    }
     .card{
       background-color: black;
       border: 1px solid #222833;
       max-width: 300px !important;
-      padding: 20px;
+      padding: 25px;
+      font-size: 2.8rem;
+      font-weight: 600;
+      border-radius: 40px;
+      transition: all 1s;
+      h5{
+        color: $my-lightgreen;
+        font-size: 1rem;
+        font-weight: 600;
+      }
     }
   }
 </style>
