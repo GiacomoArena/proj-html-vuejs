8cf622e8c90c58372ecfa228906e42a541418829
GiacomoArena <giacomoarena28@gmail.com>
2023-05-10 19:22:08 +0200

    update commit

--- src/components/partials/LatestNews.vue
+++ src/components/partials/LatestNews.vue
@@ -5,7 +5,20 @@
     name:'LatestNews',
     data(){
       return{
-        
+        cards:[
+          {
+            description:'Top 5 Most Important Aspects Of Your Game ',
+            src:'/public/blog-item-2.png'
+          },
+          {
+            description:'The Rise Of Fantasy Sports And Online Games',
+            src:'/public/blog-item-3.png'
+          },
+          {
+            description:'Benefits Of Gaming - PC And Video Games',
+            src:'/public/blog-item-4.png'
+          },
+        ]
       }
     }
   }
@@ -26,12 +39,29 @@
 
   <div class="cards-containers">
     <div class="big-card">
-      aaaaaaaa
+      <img src="../../assets/blog-item-1.png" alt="user">
     </div>
     <div class="side-container">
-      <div class="card">aaa</div>
-      <div class="card">bbb</div>
-      <div class="card">cccc</div>
+
+      <div class="cards d-flex" v-for="(card,i) in cards" :key="i">
+        <img class="player" :src="card.src" alt="user">
+        <div class="text-side">
+          
+          <span>
+            <img src="../../assets/icon/clock.png" >
+            10 Min
+            <img src="../../assets/icon/eye.png">
+            240
+            <img src="../../assets/icon/calendar.png">
+            Apr 11,2022
+          </span>
+          <p>
+            {{card.description}}
+          </p>
+        </div>
+
+      </div>
+
     </div>
   </div>
 
@@ -42,7 +72,7 @@
 <style lang="scss" scoped>
 @use '../../scss/partials/variables' as*;
   section{
-    padding-bottom: 200px;
+    padding-bottom: 300px;
     font-family: $primary-font;
     background-color: black;
     display: flex;
@@ -72,11 +102,41 @@
       display: flex;
       .big-card{
         background-color: $my-blue;
-        width: 400px;
-        height: 500px;
+        border-radius: 20px;
+        max-width: 640px;
+        min-width: 300px;
+        height: 600px;
+        img{
+          width: 100%;
+          object-fit: cover;
+        }
       }
-      .card{
+      .cards{
+        margin-left: 20px;
+        margin-bottom: 20px;
         background-color: $my-blue;
+        display: flex;
+        align-items: center;
+        border-radius: 20px;
+        width: 600px;
+        img.player{
+          border-radius: 20px;
+          object-fit: cover;
+        }
+        .text-side{
+          img{
+            margin-left: 20px;
+          }
+          span{
+            color: $my-lightgreen;
+          }
+          p{
+            margin-top: 15px;
+            font-size: 1.5rem;
+            color: white;
+            font-weight: 600;
+          }
+        }
       }
     }
 
