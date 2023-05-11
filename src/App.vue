12f1d1c53790c30ea10302f0706f29badb04cfa4
GiacomoArena <giacomoarena28@gmail.com>
2023-05-11 09:38:08 +0200

    integrated loading

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,20 @@ export default {
 },
 data(){
       return{
-        store
+        store,
+        loading:true
       }
     },
+    methods:{
+      loadingPage(){
+        setTimeout(() => {
+          this.loading = false
+        }, 2000);
+      }
+    },
+    created(){
+      this.loadingPage()
+    }
 }
 
 
@@ -26,19 +37,32 @@ data(){
 
 
 <template>
+  <section class="preloader" 
+  v-if="loading === true">
+  <img src="./assets/preloader.gif" alt="preloader">
+  </section>
 
-  <Header />
 
-  <Jumbotron />
-
-  <Main />
-
-  <Footer />
+  <section v-else>
+    <Header />
+  
+    <Jumbotron />
+  
+    <Main />
+  
+    <Footer />
+  </section>
   
 </template>
 
 <style lang="scss">
 @import '../src/scss/Main.scss'  ;
-
+.preloader{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #1B253B;
+}
 
 </style>
